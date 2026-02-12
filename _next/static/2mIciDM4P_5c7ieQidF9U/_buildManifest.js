ed8b764980369710e1120c3bc981b0f9059abe2e
AlessioDiaz0 <alessio.diaz0@gmail.com>
2026-02-12 21:51:33 +0100

    Updates

--- _next/static/2mIciDM4P_5c7ieQidF9U/_buildManifest.js
+++ _next/static/2mIciDM4P_5c7ieQidF9U/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/alessiodiaz.github.io//_next/:path+",
+        "destination": "/alessiodiaz.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
